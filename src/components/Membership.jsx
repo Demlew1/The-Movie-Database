cf2d63f98af5249bef0bd9caf073865ac4c85a45
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-19 01:38:23 +0300

    finished styling the membership component

--- src/components/Membership.jsx
+++ src/components/Membership.jsx
@@ -3,32 +3,236 @@ function Membership() {
     <div className="bg-gray-100 pt-4 text-center">
       <div className="font-['Ubuntu']">
         <p className="font-bold text-3xl text-center">Demlix Membership</p>
-        <p className="font-['Rubik'] pt-4">
+        <p className="font-['Rubik'] pt-4 font-light">
           Choose the perfect subscription for your streaming needs
         </p>
       </div>
-      <div className="bg-white font-['Montserrat'] text-xs">
-        <div className="flex flex-row gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
-            />
-          </svg>
-          <p className="">Standard</p>
+      <div className="flex flex-row gap-24 justify-center">
+        <div className="bg-white font-['Montserrat'] text-xs inline-flex flex-col gap-4 justify-center my-20 p-4 shadow-xs">
+          <div className=" flex flex-row gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+              />
+            </svg>
+            <p className="self-center">Standard</p>
+          </div>
+          <p className="text-2xl font-bold self-start ">
+            $ 14.99 <span className="text-xs font-light">/ month</span>
+          </p>
+          <button className="border-1 w-85 cursor-pointer hover:bg-gray-700 hover:text-white p-2 text-xs">
+            Select this plan
+          </button>
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-row gap-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+              <p>Stream movies in high-definition (HD) quality</p>
+            </div>
+            <div className="flex flex-row gap-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+              <p>Access to our vast library of movies and TV shows</p>
+            </div>
+            <div className="flex flex-row gap-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+              <p>Watch movies on up to two devices simultaneously</p>
+            </div>
+            <div className="flex flex-row gap-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+              <p>Cancel anytime</p>
+            </div>
+            <div className="flex flex-row gap-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+              <p>Ads-Free</p>
+            </div>
+          </div>
         </div>
-        <p className="text-xl font-bold ">
-          $ 14.99 <span className="text-xs font-light">/ month</span>
-        </p>
-        <button className="border-1 w-60 p-1 text-xs">Select this plan</button>
+        <div className="bg-white font-['Montserrat'] text-xs inline-flex flex-col gap-4 justify-center my-20 p-4 shadow-xs border-1 border-gray-600">
+          <div className=" flex flex-row gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 3.75v16.5M2.25 12h19.5M6.375 17.25a4.875 4.875 0 0 0 4.875-4.875V12m6.375 5.25a4.875 4.875 0 0 1-4.875-4.875V12m-9 8.25h16.5a1.5 1.5 0 0 0 1.5-1.5V5.25a1.5 1.5 0 0 0-1.5-1.5H3.75a1.5 1.5 0 0 0-1.5 1.5v13.5a1.5 1.5 0 0 0 1.5 1.5Zm12.621-9.44c-1.409 1.41-4.242 1.061-4.242 1.061s-.349-2.833 1.06-4.242a2.25 2.25 0 0 1 3.182 3.182ZM10.773 7.63c1.409 1.409 1.06 4.242 1.06 4.242S9 12.22 7.592 10.811a2.25 2.25 0 1 1 3.182-3.182Z"
+              />
+            </svg>
+
+            <p className="self-center">Premium</p>
+          </div>
+          <p className="text-2xl font-bold self-start ">
+            $ 19.99 <span className="text-xs font-light">/ month</span>
+          </p>
+          <button className="border-1 w-85 cursor-pointer hover:bg-white hover:text-black bg-gray-700 text-white p-2 text-xs">
+            Select this plan
+          </button>
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-row gap-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+              <p>Stream movies in 4K Ultra HD quality</p>
+            </div>
+            <div className="flex flex-row gap-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+              <p>Exclusive access to premium content and early release</p>
+            </div>
+            <div className="flex flex-row gap-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+              <p>Watch movies on up to four devices simultaneously</p>
+            </div>
+            <div className="flex flex-row gap-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+              <p>Cancel anytime</p>
+            </div>
+            <div className="flex flex-row gap-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+              <p>Ads-Free</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
