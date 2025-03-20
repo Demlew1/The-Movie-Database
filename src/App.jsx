e1181090cabe6c5ee0447a07625f924cb7f6ff90
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-20 13:24:08 +0300

    finished styling the watchlist section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import SignUp from "./components/SignUp";
 import HomePage from "./components/HomePage";
 import MovieDetails from "./components/MovieDetails";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import WatchList from "./components/WatchList";
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <MovieDetails />
+      {/* <MovieDetails /> */}
+      {/* <HomePage /> */}
+      <WatchList />
     </QueryClientProvider>
   );
   // return <HomePage />;
---
+++ src/components/WatchList.jsx
@@ -0,0 +1,52 @@
+import NavBar from "./NavBar";
+import { useQuery } from "@tanstack/react-query";
+import star from "../assets/images/star.svg";
+const apiKey = "387501be";
+async function fetchData() {
+  const response = await fetch(
+    `http://www.omdbapi.com/?apikey=${apiKey}&s=movie`
+  );
+  if (!response.ok) throw new Error("failed to fetch your data");
+  return response.json();
+}
+function WatchList() {
+  const { data } = useQuery({
+    queryKey: "watchList",
+    queryFn: fetchData,
+  });
+  return (
+    <div className="bg-gray-800 h-screen p-8 overflow-y-scroll">
+      <NavBar />
+      <div className="bg-gray-600 p-12 rounded-md m-8 flex flex-col gap-6">
+        {data.Search.map((data) => (
+          <div className="font-['Montserrat'] bg-gray-500 p-1 px- flex flex-row gap-36 items-center rounded-sm pr-4 cursor-pointer transition delay-5 hover:bg-gray-700">
+            <div className="flex flex-row gap-4 items-center">
+              <img className="w-48 h-24" src={data.Poster} />
+              <div>
+                <p className="text-xs font-bold text-gray-100">{data.Title}</p>
+                <p className="w-40 text-[10px] text-gray-100 mt-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </div>
+            </div>
+            <p className="text-xs text-gray-100">8.8/10</p>
+            <p className="text-xs text-gray-100">148min</p>
+
+            <div className="flex flex-row">
+              <img className="size-5 " src={star} alt="star-image" />
+              <img className="size-5 " src={star} alt="star-image" />
+              <img className="size-5 " src={star} alt="star-image" />
+              <img className="size-5 " src={star} alt="star-image" />
+              <img className="size-5 " src={star} alt="star-image" />
+            </div>
+            <buton className="font-bold text-[10px] text-gray-300 rounded-md bg-gray-800 p-1 hover:bg-gray-600">
+              Remove
+            </buton>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default WatchList;
