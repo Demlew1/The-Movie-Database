888bf8b94fbd927cd2c529fdaadd58b76874570a
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-27 01:56:57 +0300

    added message when the watchList is empty

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -8,40 +8,48 @@ function WatchList() {
   return (
     <div className="bg-gray-800 h-screen p-8 overflow-y-scroll">
       <NavBar />
-      <div className="bg-gray-600 p-12 rounded-md m-8 flex flex-col gap-6">
-        {watchList.map((movie) => (
-          <div
-            key={movie.imdbID}
-            className="font-['Montserrat'] bg-gray-500 p-1 px- flex flex-row gap-36 items-center rounded-sm pr-4 cursor-pointer transition delay-5 hover:bg-gray-700"
-          >
-            <div className="flex flex-row gap-4 items-center">
-              <img className="w-48 h-24" src={movie.Poster} />
-              <div>
-                <p className="text-xs font-bold text-gray-100">{movie.Title}</p>
-                <p className="w-40 text-[10px] text-gray-100 mt-2">
-                  {movie.Genre}
-                </p>
-              </div>
-            </div>
-            <p className="text-xs text-gray-100">{movie.imdbRating}</p>
-            <p className="text-xs w-20 text-gray-100">{movie.Runtime}</p>
-
-            <div className="flex flex-row">
-              <img className="size-5 " src={star} alt="star-image" />
-              <img className="size-5 " src={star} alt="star-image" />
-              <img className="size-5 " src={star} alt="star-image" />
-              <img className="size-5 " src={star} alt="star-image" />
-              <img className="size-5 " src={star} alt="star-image" />
-            </div>
-            <button
-              onClick={() => removeFromList(movie.imdbID)}
-              className="font-bold text-[10px] text-gray-300 rounded-md bg-gray-800 p-1 hover:bg-gray-600"
+      {watchList.length > 0 ? (
+        <div className="bg-gray-600 p-12 rounded-md m-8 flex flex-col gap-6">
+          {watchList.map((movie) => (
+            <div
+              key={movie.imdbID}
+              className="font-['Montserrat'] bg-gray-500 p-1 px- flex flex-row gap-36 items-center rounded-sm pr-4 cursor-pointer transition delay-5 hover:bg-gray-700"
             >
-              Remove
-            </button>
-          </div>
-        ))}
-      </div>
+              <div className="flex flex-row gap-4 items-center">
+                <img className="w-48 h-24" src={movie.Poster} />
+                <div>
+                  <p className="text-xs font-bold text-gray-100">
+                    {movie.Title}
+                  </p>
+                  <p className="w-40 text-[10px] text-gray-100 mt-2">
+                    {movie.Genre}
+                  </p>
+                </div>
+              </div>
+              <p className="text-xs text-gray-100">{movie.imdbRating}</p>
+              <p className="text-xs w-20 text-gray-100">{movie.Runtime}</p>
+
+              <div className="flex flex-row">
+                <img className="size-5 " src={star} alt="star-image" />
+                <img className="size-5 " src={star} alt="star-image" />
+                <img className="size-5 " src={star} alt="star-image" />
+                <img className="size-5 " src={star} alt="star-image" />
+                <img className="size-5 " src={star} alt="star-image" />
+              </div>
+              <button
+                onClick={() => removeFromList(movie.imdbID)}
+                className="font-bold text-[10px] text-gray-300 rounded-md bg-gray-800 p-1 hover:bg-gray-600"
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="font-mono text-amber-200 mt-40 ml-16 flex flex-row justify-center">
+          Your watchlist is empty – let's find something amazing!
+        </p>
+      )}
     </div>
   );
 }
