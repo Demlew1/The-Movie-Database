6532241c0ec38bb5b8c14b2f6e6a9efa9a2463e7
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-27 00:14:36 +0300

    working on add to watchList feature

--- src/components/MovieData.jsx
+++ src/components/MovieData.jsx
@@ -4,10 +4,16 @@ import { useNavigate } from "react-router-dom";
 import { getMovie } from "../api/api";
 function MovieData() {
   const navigate = useNavigate();
-  const { data, isLoading, isError, error } = useQuery({
+  const {
+    data: movie,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ["movies"],
     queryFn: getMovie,
   });
+
   if (isLoading)
     return (
       <p className="text-center text-white font-mono h-screen">Loading...</p>
@@ -18,21 +24,21 @@ function MovieData() {
         {error.message}
       </p>
     );
-  console.log(data);
+  console.log(movie);
   return (
     <div>
       <div className="flex flex-row flex-wrap gap-4 justify-center  pt-8">
-        {data.Search.map((data) => (
+        {movie.Search.map((movie) => (
           <div
-            key={data.imdbID}
+            key={movie.imdbID}
             className="bg-gray-700 shadow-lg text-gray-200 cursor-pointer p-2 rounded-md font-['Montserrat']  flex flex-col"
           >
-            <img className="size-60" src={data.Poster} alt="Movie poster" />
-            <p className="font-bold text-md pt-3 ">{data.Title}</p>
-            <p className="text-xs mt-2">{data.Year}</p>
+            <img className="size-60" src={movie.Poster} alt="Movie poster" />
+            <p className="font-bold text-md pt-3 ">{movie.Title}</p>
+            <p className="text-xs mt-2">{movie.Year}</p>
             <div className="pt-4 self-center">
               <button
-                onClick={() => navigate(`/movies/${data.imdbID}`)}
+                onClick={() => navigate(`/movies/${movie.imdbID}`)}
                 className=" cursor-pointer font-['Montserrat'] text-xs w-60 text-gray-100 p-2 bg-gray-900 rounded-sm hover:bg-gray-950 transition delay-10"
               >
                 View details
