45a06bc3397afc4fd50088db887b19e28350bb06
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-28 14:28:47 +0300

    finished with react-hook-form for signUp

--- src/components/signUpForm.jsx
+++ src/components/signUpForm.jsx
@@ -12,38 +12,62 @@ const validationSchema = Yup.object({
     .min(8, "Password must be at least 8 characters"),
   phone: Yup.string().required("Phone Number is required"),
   location: Yup.string().required("Your Location is required"),
-  about: Yup.string().required("Tell us about yourself"),
 }).required();
 const signUpForm = [
   {
     label: "Full Name",
     placeHolder: "Your Full Name",
     type: "text",
+    name: "fullName",
   },
   {
     label: "Email",
     placeHolder: "example@gmail.com",
     type: "email",
+    name: "email",
   },
   {
     label: "Phone number",
     placeHolder: "Your Phone Number",
     type: "number",
+    name: "phone",
   },
   {
     label: "Password",
     placeHolder: "Enter atleast 8+ characters",
     type: "password",
+    name: "password",
   },
   {
     label: "Location",
     placeHolder: "-Select Your Country-",
     type: "text",
+    name: "location",
   },
 ];
 function SignUpForm() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      phone: "",
+      location: "",
+    },
+  });
+  function submit(data) {
+    console.log("data", data);
+    console.log("errors", errors);
+    reset();
+  }
   return (
-    <form className="flex flex-col gap-2">
+    <form onSubmit={handleSubmit(submit)} className="flex flex-col gap-2">
       {signUpForm.map((data) => (
         <div
           className="font-['Montserrat'] flex flex-col gap-1"
@@ -53,10 +77,16 @@ function SignUpForm() {
             {data.label}
           </label>
           <input
+            {...register(data.name)}
             className="text-[10px] p-2 focus:outline-none border-1 w-100 border-gray-400 "
             type={data.type}
             placeholder={data.placeHolder}
           />
+          {errors[data.name] && (
+            <span className="text-[10px] text-red-500">
+              {errors[data.name].message}
+            </span>
+          )}
         </div>
       ))}
       <div className=" font-['Montserrat'] flex flex-col gap-1">
@@ -71,13 +101,12 @@ function SignUpForm() {
         <button className="font-['montserrat'] text-xs text-gray-700 cursor-pointer hover:text-gray-900 transition delay-25">
           Cancel
         </button>
-        <Link
-          to="/Home"
+        <button
           className="text-center bg-gray-600 text-gray-100 text-xs w-24 h-8 self-end p-2 cursor-pointer font-bold hover:bg-gray-500 transition delay-10 "
           type="submit"
         >
           Sign up
-        </Link>
+        </button>
       </div>
     </form>
   );
