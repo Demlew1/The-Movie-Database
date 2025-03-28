fc0b11dcd9ee80a75cf67c8ee697506a0f99b8d4
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-28 13:31:52 +0300

    finished with showPassword state

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,6 +4,8 @@ import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import supabase from "../store/auth";
 import { useState } from "react";
+import { IoMdEye } from "react-icons/io";
+import { IoEyeOff } from "react-icons/io5";
 const validationSchema = Yup.object({
   email: Yup.string()
     .required("Email is required")
@@ -16,6 +18,7 @@ function SignIn() {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const [authError, setAuthError] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
   const {
     register,
     handleSubmit,
@@ -97,25 +100,17 @@ function SignIn() {
                 <input
                   {...register("password")}
                   className="font-['montserrat'] text-xs w-60 focus:outline-none sm:text-[10px] md:text-xs"
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   name="password"
                   autoComplete="current-password"
                   placeholder="Enter atleast 8+ characters"
                 />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-3 cursor-pointer"
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
-                  />
-                </svg>
+                  {showPassword ? <IoMdEye /> : <IoEyeOff />}
+                </button>
               </div>
             </div>
             {errors.password && (
