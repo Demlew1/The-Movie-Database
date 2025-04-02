a7189e7171f43575754f78eeb37bbb89b3596776
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-04-02 12:23:10 +0300

    reduced the membership codeling using array map method

--- src/components/pages/Home-page/Membership.jsx
+++ src/components/pages/Home-page/Membership.jsx
@@ -1,3 +1,22 @@
+import { IoCheckmark } from "react-icons/io5";
+import { RiFlashlightFill } from "react-icons/ri";
+import { FaCrown } from "react-icons/fa";
+import { FaDollarSign } from "react-icons/fa";
+const standard = [
+  { message: "Stream movies in high-definition (HD) quality", id: 1 },
+  { message: "Access to our vast library of movies and TV shows", id: 2 },
+  { message: "Watch movies on up to two devices simultaneously", id: 3 },
+  { message: "Cancel anytime", id: 4 },
+  { message: "Ads-Free", id: 5 },
+];
+const premium = [
+  { message: "Stream movies in 4K Ultra HD quality", id: 1 },
+  { message: "Exclusive access to premium content and early release", id: 2 },
+  { message: "Watch movies on up to four devices simultaneously", id: 3 },
+  { message: "Cancel anytime", id: 4 },
+  { message: "Ads-Free", id: 5 },
+];
+
 function Membership() {
   return (
     <div className="bg-gray-100 pt-12 text-center">
@@ -10,227 +29,55 @@ function Membership() {
       <div className="flex flex-row gap-24 justify-center">
         <div className="bg-white font-['Montserrat'] text-xs inline-flex flex-col gap-4 justify-center my-20 p-4 shadow-xs">
           <div className=" flex flex-row gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
-              />
-            </svg>
+            <RiFlashlightFill className="size-5" />
             <p className="self-center">Standard</p>
           </div>
-          <p className="text-2xl font-bold self-start ">
-            $ 14.99 <span className="text-xs font-light">/ month</span>
-          </p>
+          <div className="text-2xl font-bold self-start flex flex-row gap-1 items-center">
+            <FaDollarSign />
+            <p className="text-xs font-light">
+              <span className="font-bold text-2xl">14.99</span>/ month
+            </p>
+          </div>
           <button className="border-1 w-85 cursor-pointer hover:bg-gray-700 hover:text-white p-2 text-xs">
             Select this plan
           </button>
           <div className="flex flex-col gap-4">
-            <div className="flex flex-row gap-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-4"
+            {standard.map((message) => (
+              <div
+                className="flex flex-row items-center gap-4"
+                key={message.id}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 12.75 6 6 9-13.5"
-                />
-              </svg>
-              <p>Stream movies in high-definition (HD) quality</p>
-            </div>
-            <div className="flex flex-row gap-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 12.75 6 6 9-13.5"
-                />
-              </svg>
-              <p>Access to our vast library of movies and TV shows</p>
-            </div>
-            <div className="flex flex-row gap-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 12.75 6 6 9-13.5"
-                />
-              </svg>
-              <p>Watch movies on up to two devices simultaneously</p>
-            </div>
-            <div className="flex flex-row gap-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 12.75 6 6 9-13.5"
-                />
-              </svg>
-              <p>Cancel anytime</p>
-            </div>
-            <div className="flex flex-row gap-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 12.75 6 6 9-13.5"
-                />
-              </svg>
-              <p>Ads-Free</p>
-            </div>
+                <IoCheckmark className="size-5" />
+                <p>{message.message}</p>
+              </div>
+            ))}
           </div>
         </div>
         <div className="bg-white font-['Montserrat'] text-xs inline-flex flex-col gap-4 justify-center my-20 p-4 shadow-xs border-1 border-gray-600">
           <div className=" flex flex-row gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 3.75v16.5M2.25 12h19.5M6.375 17.25a4.875 4.875 0 0 0 4.875-4.875V12m6.375 5.25a4.875 4.875 0 0 1-4.875-4.875V12m-9 8.25h16.5a1.5 1.5 0 0 0 1.5-1.5V5.25a1.5 1.5 0 0 0-1.5-1.5H3.75a1.5 1.5 0 0 0-1.5 1.5v13.5a1.5 1.5 0 0 0 1.5 1.5Zm12.621-9.44c-1.409 1.41-4.242 1.061-4.242 1.061s-.349-2.833 1.06-4.242a2.25 2.25 0 0 1 3.182 3.182ZM10.773 7.63c1.409 1.409 1.06 4.242 1.06 4.242S9 12.22 7.592 10.811a2.25 2.25 0 1 1 3.182-3.182Z"
-              />
-            </svg>
+            <FaCrown className="size-5" />
 
             <p className="self-center">Premium</p>
           </div>
-          <p className="text-2xl font-bold self-start ">
-            $ 19.99 <span className="text-xs font-light">/ month</span>
-          </p>
+          <div className="text-2xl font-bold self-start flex flex-row gap-1 items-center">
+            <FaDollarSign />
+            <p className="text-xs font-light">
+              <span className="font-bold text-2xl">19.99</span>/ month
+            </p>
+          </div>
           <button className="border-1 w-85 cursor-pointer hover:bg-white hover:text-black bg-gray-700 text-white p-2 text-xs">
             Select this plan
           </button>
           <div className="flex flex-col gap-4">
-            <div className="flex flex-row gap-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-4"
+            {premium.map((message) => (
+              <div
+                className="flex flex-row items-center gap-4"
+                key={message.id}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 12.75 6 6 9-13.5"
-                />
-              </svg>
-              <p>Stream movies in 4K Ultra HD quality</p>
-            </div>
-            <div className="flex flex-row gap-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 12.75 6 6 9-13.5"
-                />
-              </svg>
-              <p>Exclusive access to premium content and early release</p>
-            </div>
-            <div className="flex flex-row gap-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 12.75 6 6 9-13.5"
-                />
-              </svg>
-              <p>Watch movies on up to four devices simultaneously</p>
-            </div>
-            <div className="flex flex-row gap-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 12.75 6 6 9-13.5"
-                />
-              </svg>
-              <p>Cancel anytime</p>
-            </div>
-            <div className="flex flex-row gap-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 12.75 6 6 9-13.5"
-                />
-              </svg>
-              <p>Ads-Free</p>
-            </div>
+                <IoCheckmark className="size-5" />
+                <p>{message.message}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
