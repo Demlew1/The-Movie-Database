ea8303665bc06c28b07531206297dca70e345e61
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-04-03 19:54:24 +0300

    finished with the responsiveness of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,18 +5,20 @@ import { FaInstagram } from "react-icons/fa";
 import { CiAt } from "react-icons/ci";
 function Footer() {
   return (
-    <div className="pt-10 flex flex-col gap-2 items-center justify-center pb-8 bg-white">
-      <div className="flex flex-row gap-4 pb-4">
-        <FaLinkedin className="cursor-pointer  size-4" />
-        <FaGithub className="cursor-pointer size-4" />
-        <FaSquareXTwitter className="cursor-pointer size-4" />
-        <FaInstagram className="cursor-pointer size-4" />
+    <div className="pt-5 flex flex-col gap-2 items-center justify-center pb-4 bg-white">
+      <div className="flex flex-row gap-4 pb-2">
+        <FaLinkedin className="cursor-pointer  size-3" />
+        <FaGithub className="cursor-pointer size-3" />
+        <FaSquareXTwitter className="cursor-pointer size-3" />
+        <FaInstagram className="cursor-pointer size-3" />
       </div>
       <div className="flex flex-row gap-1 items-center font-['Rubik'] text-xs">
-        <CiAt className="size-5" />
-        <p className="">DEMLIX Media Direct, LLC. All rights reserved</p>
+        <CiAt className="size-4" />
+        <p className="text-[10px]">
+          DEMLIX Media Direct, LLC. All rights reserved
+        </p>
       </div>
-      <div className="flex flex-row gap-8 font-['Rubik'] text-xs">
+      <div className="flex flex-row gap-8 font-['Rubik'] text-[10px]">
         <button className="text-gray-600 cursor-pointer">Terms</button>
         <button className="text-gray-600 cursor-pointer">Privacy</button>
         <button className="text-gray-600 cursor-pointer">Help</button>
