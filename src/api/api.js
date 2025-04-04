2e4c34c0ec4ef9e7fb3880a8c1512d8a3f83b086
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-04-04 04:08:43 +0300

    finished the project

--- src/api/api.js
+++ src/api/api.js
@@ -1,21 +1,22 @@
+const apiKey = "387501be";
 import supabase from "../store/auth";
 export async function getMovie(page = 1) {
   const response = await fetch(
-    `http://www.omdbapi.com/?apikey=387501be&s=movie&page=${page}`
+    `http://www.omdbapi.com/?apikey=${apiKey}&s=movie&page=${page}`
   );
   if (!response.ok) throw new Error("Failed to fetch the data");
   return response.json();
 }
 export async function getMovieDetails(id) {
   const response = await fetch(
-    `http://www.omdbapi.com/?apikey=387501be&i=${id}&plot=full`
+    `http://www.omdbapi.com/?apikey=${apiKey}&i=${id}&plot=full`
   );
   if (!response.ok) throw new Error("Error fetching the movie data");
   return response.json();
 }
 export async function searchMovies(query) {
   const response = await fetch(
-    `http://www.omdbapi.com/?apikey=387501be&s=${query}`
+    `http://www.omdbapi.com/?apikey=${apiKey}&s=${query}`
   );
   if (!response.ok) throw new Error("Failed to fetch search results");
   return response.json();
